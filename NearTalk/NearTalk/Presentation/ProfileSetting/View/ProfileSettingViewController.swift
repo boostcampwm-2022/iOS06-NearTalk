40e9ea2139ac9eed0d99b5c2540fff4ec2a49df5
prestonk162 <prestonk162@gmail.com>
2022-11-15 02:17:06 +0900

    [Feature] 프로필 수정 UI 구현

---
+++ NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ProfileSettingViewController.swift
+//  NearTalk
+//
+//  Created by Preston Kim on 2022/11/14.
+//
+
+import RxCocoa
+import SnapKit
+import Then
+import UIKit
+
+final class ProfileSettingViewController: UIViewController {
+    private let profileImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.isUserInteractionEnabled = true
+        $0.backgroundColor = .lightGray
+    }
+
+    private let nicknameField = UITextField().then {
+        $0.placeholder = "닉네임"
+        $0.font = UIFont.systemFont(ofSize: 30)
+    }
+    
+    private let messageField = UITextField().then {
+        $0.placeholder = "상태 메세지"
+        $0.font = UIFont.systemFont(ofSize: 30)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        configureConstraint()
+    }
+}
+
+private extension ProfileSettingViewController {
+    func configureUI() {
+        configureNavigationBar()
+        [profileImageView, nicknameField, messageField].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .systemGray5
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.title = "프로필 설정"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "등록", style: .plain, target: self, action: nil)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .bold)]
+    }
+    
+    func configureConstraint() {
+        profileImageView.snp.makeConstraints { (make) in
+            make.horizontalEdges.top.equalToSuperview()
+            make.width.equalTo(profileImageView.snp.height)
+        }
+        
+        nicknameField.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(profileImageView.snp.bottom).offset(10)
+//            make.height.equalTo(nicknameField.snp.width).multipliedBy(0.15)
+        }
+        
+        messageField.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(nicknameField.snp.bottom).offset(10)
+//            make.bottom.equalToSuperview().offset(20)
+        }
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+// swiftlint:disable: type_name
+struct ProfileSettingViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        UINavigationController(rootViewController: ProfileSettingViewController()).showPreview(.iPhone14Pro)
+        UINavigationController(rootViewController: ProfileSettingViewController()).showPreview(.iPhoneSE3)
+    }
+}
+#endif
