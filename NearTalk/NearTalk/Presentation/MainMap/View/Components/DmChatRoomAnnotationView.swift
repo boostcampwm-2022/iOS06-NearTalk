363767a7e5d29a87ceb66b55f28325c7dfada411
lymchgmk <lymchgmk@gmail.com>
2022-11-15 18:07:05 +0900

    [Feature] 채팅방 종류 별  Annotation 작성
    - Dm: 1대 1 chatRoom
    - Open: 오픈 chatRoom

---
+++ NearTalk/NearTalk/Presentation/MainMap/View/Components/DmChatRoomAnnotationView.swift
@@ -0,0 +1,28 @@
+//
+//  ChatRoomDmAnnotationView.swift
+//  NearTalk
+//
+//  Created by lymchgmk on 2022/11/15.
+//
+
+import MapKit
+
+final class DmChatRoomAnnotationView: MKMarkerAnnotationView {
+    static let reuseIdentifier = "OpenChatRoomAnnotationView"
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        // clusteringIdentifier = "clustering"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        displayPriority = .defaultLow
+        markerTintColor = UIColor.cyan // 임시 색상
+        glyphImage = UIImage(systemName: "figure.wave") // 임시 이미지
+    }
+}
---
+++ NearTalk/NearTalk/Presentation/MainMap/View/Components/OpenChatRoomAnnotationView.swift
@@ -0,0 +1,28 @@
+//
+//  ChatRoomOpenAnnotationView.swift
+//  NearTalk
+//
+//  Created by lymchgmk on 2022/11/15.
+//
+
+import MapKit
+
+final class OpenChatRoomAnnotationView: MKMarkerAnnotationView {
+    static let reuseIdentifier = "OpenChatRoomAnnotationView"
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        // clusteringIdentifier = "clustering"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        displayPriority = .defaultLow
+        markerTintColor = UIColor.red // 임시 색상
+        glyphImage = UIImage(systemName: "figure.2.arms.open") // 임시 이미지
+    }
+}
