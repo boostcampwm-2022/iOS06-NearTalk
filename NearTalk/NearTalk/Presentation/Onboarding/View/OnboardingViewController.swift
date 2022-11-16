5d54a5ae7e5b174400619e4a12cd0e7c9ce01178
prestonk162 <prestonk162@gmail.com>
2022-11-16 13:15:34 +0900

    [Fix] 앱 실행시 배경 화면 검은 버그 해결

--- NearTalk/NearTalk/Presentation/Onboarding/View/OnboardingViewController.swift
+++ NearTalk/NearTalk/Presentation/Onboarding/View/OnboardingViewController.swift
@@ -60,6 +60,7 @@ final class OnboardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         self.addSubViews()
         self.configureConstraints()
         self.bindToViewModel()
