10da88204ef8df382ccfe6ea7be39166baa13e1c
prestonk162 <prestonk162@gmail.com>
2022-11-17 13:07:40 +0900

    [Chore] ProfileSettingViewController 코딩 컨벤션에 맞게 수정

--- NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingViewController.swift
+++ NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingViewController.swift
@@ -13,6 +13,7 @@ import Then
 import UIKit
 
 final class ProfileSettingViewController: UIViewController {
+    // MARK: - UI properties
     private let profileImageView = UIImageView().then {
         $0.contentMode = .scaleAspectFill
         $0.isUserInteractionEnabled = true
@@ -29,8 +30,11 @@ final class ProfileSettingViewController: UIViewController {
         $0.font = UIFont.systemFont(ofSize: 30)
     }
     
+    // MARK: - Properties
     private weak var coordinator: MyProfileCoordinator?
+    private let disposeBag: DisposeBag = DisposeBag()
 
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
@@ -46,10 +50,9 @@ final class ProfileSettingViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private let disposeBag: DisposeBag = DisposeBag()
 }
 
+// MARK: - Helpers
 private extension ProfileSettingViewController {
     func configureUI() {
         configureNavigationBar()
