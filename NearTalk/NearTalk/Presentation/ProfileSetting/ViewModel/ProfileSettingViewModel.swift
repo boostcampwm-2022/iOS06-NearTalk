672fe53eba284a42096b5374ced7ee29979d6f53
prestonk162 <prestonk162@gmail.com>
2022-11-21 17:33:34 +0900

    [Chore] ProfileSettingViewModel 파일 추가

--- NearTalk/NearTalk/Presentation/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
+++ NearTalk/NearTalk/Presentation/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
@@ -8,14 +8,14 @@
 import RxSwift
 import RxCocoa
 
-protocol ProfileSettingAction {
+protocol ProfileSettingViewModelAction {
     var presentImagePicker: () -> Single<Data?> { get }
-    var backToMyProfile: () -> Void { get }
 }
 
 protocol ProfileSettingInput {
     var nickName: Observable<String> { get }
     var message: Observable<String> { get }
+    var register: Observable<Void> { get }
 }
 
 protocol ProfileSettingOutput {
@@ -25,21 +25,79 @@ protocol ProfileSettingOutput {
 }
 
 protocol ProfileSettingViewModel {
-    init(useCase: any UpdateProfileUseCase, action: any ProfileSettingAction)
+    init(updateProfileUseCase: any UpdateProfileUseCase,
+         validateNickNameUseCase: any ValidateTextUseCase,
+         validateStatusMessageUseCase: any ValidateTextUseCase,
+         action: any ProfileSettingViewModelAction)
     func transform(_ input: ProfileSettingInput) -> ProfileSettingOutput
+    func injectProfile(_ profile: UserProfile)
+    func editImage() -> Single<Data?>
 }
 
 final class DefaultProfileSettingViewModel: ProfileSettingViewModel {
-    private let useCase: any UpdateProfileUseCase
-    private let action: any ProfileSettingAction
+    private let updateProfileUseCase: any UpdateProfileUseCase
+    private let validateNickNameUseCase: any ValidateTextUseCase
+    private let validateStatusMessageUseCase: any ValidateTextUseCase
+    private let action: any ProfileSettingViewModelAction
+    private let disposeBag: DisposeBag = DisposeBag()
 
-    init(useCase: UpdateProfileUseCase, action: ProfileSettingAction) {
-        <#code#>
+    init(updateProfileUseCase: any UpdateProfileUseCase,
+         validateNickNameUseCase: any ValidateTextUseCase,
+         validateStatusMessageUseCase: any ValidateTextUseCase,
+         action: any ProfileSettingViewModelAction) {
+        self.updateProfileUseCase = updateProfileUseCase
+        self.validateNickNameUseCase = validateNickNameUseCase
+        self.validateStatusMessageUseCase = validateStatusMessageUseCase
+        self.action = action
+    }
+    
+    struct Input: ProfileSettingInput {
+        let nickName: Observable<String>
+        let message: Observable<String>
+        let register: Observable<Void>
+        let imageEdit: Observable<Void>
+    }
+    
+    struct Output: ProfileSettingOutput {
+        let nickNameValidity: BehaviorRelay<Bool>
+        let messageValidity: BehaviorRelay<Bool>
+        let image: BehaviorRelay<Data?>
+        let registerEnable: BehaviorRelay<Bool>
     }
     
     func transform(_ input: ProfileSettingInput) -> ProfileSettingOutput {
-        <#code#>
+        let nickNameValidity: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+        let messageValidity: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+        let registerEnable: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+        input.nickName
+            .map {
+                self.validateNickNameUseCase.execute($0)
+            }
+            .bind(to: nickNameValidity)
+            .disposed(by: self.disposeBag)
+        input.message
+            .map {
+                self.validateStatusMessageUseCase.execute($0)
+            }
+            .bind(to: messageValidity)
+            .disposed(by: self.disposeBag)
+        Observable.combineLatest(
+            nickNameValidity.asObservable(),
+            nickNameValidity.asObservable()) {
+                $0 && $1
+            }
+            .bind(to: registerEnable)
+            .disposed(by: self.disposeBag)
+        return Output(nickNameValidity: nickNameValidity,
+                      messageValidity: nickNameValidity,
+                      image: <#T##BehaviorRelay<Data?>#>,
+                      registerEnable: registerEnable)
     }
     
+    func editImage() -> Single<Data?> {
+        return self.action.presentImagePicker()
+    }
     
+    func injectProfile(_ profile: UserProfile) {
+    }
 }
