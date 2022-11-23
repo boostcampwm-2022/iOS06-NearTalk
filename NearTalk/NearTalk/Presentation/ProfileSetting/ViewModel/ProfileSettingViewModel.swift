b59ff919ff328ae3c69876d44e6feaf428d79927
prestonk162 <prestonk162@gmail.com>
2022-11-23 15:49:09 +0900

    [Feat] 프로필 수정 DIContainer, MVVM, 클린 아키텍처 구현

--- NearTalk/NearTalk/Presentation/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
+++ NearTalk/NearTalk/Presentation/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
@@ -5,105 +5,117 @@
 //  Created by Preston Kim on 2022/11/21.
 //
 
+import RxRelay
 import RxSwift
-import RxCocoa
-
-protocol ProfileSettingViewModelAction {
-    var presentImagePicker: () -> Single<Data?> { get }
-    var presentRegisterResult: () -> Void { get }
-}
 
 protocol ProfileSettingInput {
-    var nickName: Observable<String> { get }
-    var message: Observable<String> { get }
+    func editNickName(_ text: String)
+    func editStatusMessage(_ text: String)
+    func editImage()
+    func update()
 }
 
 protocol ProfileSettingOutput {
     var nickNameValidity: BehaviorRelay<Bool> { get }
     var messageValidity: BehaviorRelay<Bool> { get }
-    var registerEnable: BehaviorRelay<Bool> { get }
+    var image: BehaviorRelay<Data?> { get }
+    var updateEnable: BehaviorRelay<Bool> { get }
 }
 
-protocol ProfileSettingViewModel {
-    init(updateProfileUseCase: any UpdateProfileUseCase,
-         validateNickNameUseCase: any ValidateTextUseCase,
-         validateStatusMessageUseCase: any ValidateTextUseCase,
-         action: any ProfileSettingViewModelAction,
-         profile: UserProfile)
-    func transform(_ input: ProfileSettingInput) -> ProfileSettingOutput
-    func injectProfile(_ profile: UserProfile)
-    func editImage() -> Single<Data?>
-    func register(nickName: String, message: String, image: Data?)
+protocol ProfileSettingViewModel: ProfileSettingInput, ProfileSettingOutput {}
+
+protocol ProfileSettingViewModelAction {
+    var presentImagePicker: ((BehaviorRelay<Data?>) -> Void)? { get }
+    var presentUpdateFailure: (() -> Void)? { get }
 }
 
 final class DefaultProfileSettingViewModel: ProfileSettingViewModel {
+    func editNickName(_ text: String) {
+        self.nickName = text
+        self.nickNameValidity
+            .accept(self.validateNickNameUseCase.execute(text))
+    }
+    
+    func editStatusMessage(_ text: String) {
+        self.message = text
+        self.messageValidity
+            .accept(self.validateStatusMessageUseCase.execute(text))
+    }
+    
+    func editImage() {
+        self.action.presentImagePicker?(self.image)
+    }
+    
+    func update() {
+        if let image = self.image.value {
+            self.uploadImageUseCase.execute(image: image)
+                .subscribe(onSuccess: { [weak self] imagePath in
+                    self?.updateProfile(imagePath: imagePath)
+                }, onFailure: { [weak self] _ in
+                    self?.action.presentUpdateFailure?()
+                })
+                .disposed(by: self.disposeBag)
+        } else {
+            self.updateProfile(imagePath: nil)
+        }
+    }
+    
+    private func updateProfile(imagePath: String?) {
+        let newProfile: UserProfile = UserProfile(
+            uuid: self.profile.uuid,
+            username: self.nickName,
+            email: self.profile.email,
+            statusMessage: self.message,
+            profileImagePath: imagePath,
+            friends: self.profile.friends,
+            chatRooms: self.profile.chatRooms)
+        self.updateProfileUseCase.execute(profile: newProfile)
+            .subscribe(onCompleted: { [weak self] in
+                self?.profile = newProfile
+            }, onError: { [weak self] _ in
+                self?.action.presentUpdateFailure?()
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    let nickNameValidity: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    let messageValidity: RxRelay.BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    let image: RxRelay.BehaviorRelay<Data?> = BehaviorRelay(value: nil)
+    let updateEnable: RxRelay.BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    
     private let updateProfileUseCase: any UpdateProfileUseCase
     private let validateNickNameUseCase: any ValidateTextUseCase
     private let validateStatusMessageUseCase: any ValidateTextUseCase
+    private let uploadImageUseCase: any UploadImageUseCase
     private let action: any ProfileSettingViewModelAction
     private let disposeBag: DisposeBag = DisposeBag()
-
+    
+    private var nickName: String?
+    private var message: String?
+    private var profile: UserProfile
+    
     init(updateProfileUseCase: any UpdateProfileUseCase,
          validateNickNameUseCase: any ValidateTextUseCase,
          validateStatusMessageUseCase: any ValidateTextUseCase,
+         uploadImageUseCase: any UploadImageUseCase,
          action: any ProfileSettingViewModelAction,
          profile: UserProfile) {
         self.updateProfileUseCase = updateProfileUseCase
         self.validateNickNameUseCase = validateNickNameUseCase
         self.validateStatusMessageUseCase = validateStatusMessageUseCase
+        self.uploadImageUseCase = uploadImageUseCase
         self.action = action
+        self.nickName = profile.username
+        self.message = profile.statusMessage
+        self.profile = profile
+        self.bind()
     }
     
-    struct Input: ProfileSettingInput {
-        let nickName: Observable<String>
-        let message: Observable<String>
-    }
-    
-    struct Output: ProfileSettingOutput {
-        let nickNameValidity: BehaviorRelay<Bool>
-        let messageValidity: BehaviorRelay<Bool>
-        let image: BehaviorRelay<Data?>
-        let registerEnable: BehaviorRelay<Bool>
-    }
-    
-    func transform(_ input: ProfileSettingInput) -> ProfileSettingOutput {
-        let nickNameValidity: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-        let messageValidity: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-        let registerEnable: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-        input.nickName
-            .map {
-                self.validateNickNameUseCase.execute($0)
-            }
-            .bind(to: nickNameValidity)
-            .disposed(by: self.disposeBag)
-        input.message
-            .map {
-                self.validateStatusMessageUseCase.execute($0)
-            }
-            .bind(to: messageValidity)
-            .disposed(by: self.disposeBag)
-        Observable.combineLatest(
-            nickNameValidity.asObservable(),
-            nickNameValidity.asObservable()) {
-                $0 && $1
-            }
-            .bind(to: registerEnable)
-            .disposed(by: self.disposeBag)
-        return Output(nickNameValidity: nickNameValidity,
-                      messageValidity: nickNameValidity,
-                      image: <#T##BehaviorRelay<Data?>#>,
-                      registerEnable: registerEnable)
-    }
-    
-    func editImage() -> Single<Data?> {
-        return self.action.presentImagePicker()
-    }
-    
-    func injectProfile(_ profile: UserProfile) {
-    }
-    
-    func register(nickName: String, message: String, image: Data?) {
-        let newProfile: UserProfile = UserProfile()
-        self.updateProfileUseCase.execute(profile: <#T##UserProfile#>)
+    private func bind() {
+        Observable.combineLatest(self.nickNameValidity.asObservable(), self.messageValidity.asObservable()) {
+            $0 && $1
+        }
+        .bind(to: self.updateEnable)
+        .disposed(by: self.disposeBag)
     }
 }
