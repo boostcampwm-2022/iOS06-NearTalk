c7ffcab2e15eb53568c76b9e29575c860de6c8e1
dongeunshin <7017410@naver.com>
2022-11-24 10:13:04 +0900

    [Feat] 채팅 cell 틀 구현

--- NearTalk/NearTalk/Presentation/Chat/View/ChatCollectionViewCell.swift
+++ NearTalk/NearTalk/Presentation/Chat/View/ChatCollectionViewCell.swift
@@ -7,7 +7,44 @@
 
 import UIKit
 
+import SnapKit
+
 class ChatCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: ChatRoomListCell.self)
     
+    var message: String? {
+        didSet {
+            label.text = message
+        }
+    }
+    
+    var textAlignment: Bool? {
+        didSet {
+            label.textAlignment = textAlignment == true ?  .right : .left
+        }
+    }
+    
+    private let label: UILabel = UILabel().then { label in
+        label.text = " "
+        label.font = UIFont.systemFont(ofSize: 16)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addViews()
+        self.backgroundColor = .yellow
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        addViews()
+    }
+    
+    private func addViews() {
+        self.addSubview(label)
+        
+        self.label.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
