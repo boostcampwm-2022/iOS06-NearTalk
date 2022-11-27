20a66097a1d5d00219ffef3ab93bcf35ad0a1b3a
dongeunshin <7017410@naver.com>
2022-11-27 22:05:30 +0900

    [feat] DefalultMessagingUseCase 구현

--- NearTalk/NearTalk/Presentation/Chat/MessagingUseCase.swift
+++ NearTalk/NearTalk/Presentation/Chat/MessagingUseCase.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
+import RxSwift
+
 protocol MessagingUseCase {
-    
+    func sendMessage(message: ChatMessage, roomName: String) -> Completable
 }
 
 final class DefalultMessagingUseCase: MessagingUseCase {
+    private let chatMessageRepository: ChatMessageRepository
     
+    init(chatMessageRepository: ChatMessageRepository) {
+        self.chatMessageRepository = chatMessageRepository
+    }
+    
+    func sendMessage(message: ChatMessage, roomName: String) -> Completable {
+        return self.chatMessageRepository.sendMessage(
+            message: message,
+            roomName: roomName
+        )
+    }
 }
