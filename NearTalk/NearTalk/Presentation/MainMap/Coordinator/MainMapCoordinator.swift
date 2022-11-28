24a21f6c84de43468f0713b64a1d1d7091a0c3c1
lymchgmk <lymchgmk@gmail.com>
2022-11-29 01:59:50 +0900

    [Fix] 메인지도 Coordinator 수정

--- NearTalk/NearTalk/Presentation/MainMap/Coordinator/MainMapCoordinator.swift
+++ NearTalk/NearTalk/Presentation/MainMap/Coordinator/MainMapCoordinator.swift
@@ -26,13 +26,11 @@ final class MainMapCoordinator: Coordinator {
     }
     
     func start() {
-        let actions = MainMapViewModel.Actions(
-            showCreateChatRoomView: self.showCreateChatRoomView,
-            showBottomSheetView: self.showBottomSheetView
-        )
+        let actions = MainMapViewModel.Actions(showCreateChatRoomView: self.showCreateChatRoomView,
+                                               showBottomSheetView: self.showBottomSheetView)
         
-        self.mainMapVC = dependencies.makeMainMapViewController(actions: actions)
         self.bottomSheetVC = dependencies.makeBottomSheetViewController()
+        self.mainMapVC = dependencies.makeMainMapViewController(actions: actions)
         
         if let mainMapVC = self.mainMapVC {
             self.navigationController?.pushViewController(mainMapVC, animated: true)
@@ -41,15 +39,16 @@ final class MainMapCoordinator: Coordinator {
     
     // MARK: - Actions
     private func showCreateChatRoomView() {
-        // TODO: 채팅방 생성 뷰 보여주는 로직 추가
+        // TODO: - 채팅방 생성 뷰 보여주는 로직 추가
+        print(#function)
+        // CreateGroupChatCoordinator.start()
     }
     
     private func showBottomSheetView() {
         guard let mainMapVC = self.mainMapVC,
-              let bottomSheetVC = self.bottomSheetVC
-        else {
-            return
-        }
+              let bottomSheetVC = self.bottomSheetVC else { return }
+        
+        // TODO: - BottomSheet 데이터 주입
         
         mainMapVC.present(bottomSheetVC, animated: true)
     }
