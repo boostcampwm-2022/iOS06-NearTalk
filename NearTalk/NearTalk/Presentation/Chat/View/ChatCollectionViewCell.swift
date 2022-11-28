2cd7266e8611f89e8f285b78731e42663c2834ec
dongeunshin <7017410@naver.com>
2022-11-28 21:43:48 +0900

    [fix] import 개행 제거

--- NearTalk/NearTalk/Presentation/Chat/View/ChatCollectionViewCell.swift
+++ NearTalk/NearTalk/Presentation/Chat/View/ChatCollectionViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 import SnapKit
 
 class ChatCollectionViewCell: UICollectionViewCell {
--- NearTalk/NearTalk/Presentation/Chat/ViewModel/ChatViewModel.swift
+++ NearTalk/NearTalk/Presentation/Chat/ViewModel/ChatViewModel.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 import RxSwift
 
 protocol ChatViewModelInput {
