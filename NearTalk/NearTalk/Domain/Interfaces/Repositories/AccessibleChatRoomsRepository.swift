15672a84cfefb6ebfea01475bb3a28a2dd26f8df
lymchgmk <lymchgmk@gmail.com>
2022-11-29 01:58:56 +0900

    [Feature] Clean Architecture 구현
    UseCase, Repository 추가

---
+++ NearTalk/NearTalk/Domain/Interfaces/Repositories/AccessibleChatRoomsRepository.swift
@@ -0,0 +1,15 @@
+//
+//  AccessibleChatRoomsRepository.swift
+//  NearTalk
+//
+//  Created by lymchgmk on 2022/11/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol AccessibleChatRoomsRepository {
+    func fetchAccessibleAllChatRooms(in region: NCMapRegion) -> Single<[ChatRoom]>
+    func fetchAccessibleGroupChatRooms(in region: NCMapRegion) -> Single<[GroupChatRoomListData]>
+    func fetchAccessibleDMChatRooms(in region: NCMapRegion) -> Single<[DMChatRoomListData]>
+}
