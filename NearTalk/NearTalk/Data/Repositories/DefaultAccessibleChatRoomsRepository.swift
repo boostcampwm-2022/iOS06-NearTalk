baab4a910b2c6292d1f4b33a6ca39bb01825d88d
lymchgmk <lymchgmk@gmail.com>
2022-12-01 18:37:07 +0900

    [Feature] 더미데이터 repo 주입

--- NearTalk/NearTalk/Data/Repositories/DefaultAccessibleChatRoomsRepository.swift
+++ NearTalk/NearTalk/Data/Repositories/DefaultAccessibleChatRoomsRepository.swift
@@ -29,24 +29,20 @@ extension DefaultAccessibleChatRoomsRepository: AccessibleChatRoomsRepository {
         let centerLocation = region.centerLocation
         let southWest = centerLocation.add(longitudeDelta: -(region.longitudeDelta / 2), latitudeDelta: -(region.latitudeDelta / 2))
         let northEast = centerLocation.add(longitudeDelta: region.longitudeDelta / 2, latitudeDelta: region.latitudeDelta / 2)
-        
+
+        let service = self.dependencies.firestoreService
         let queryList: [FirebaseQueryDTO] = [
             .init(key: "latitude", value: southWest.latitude, queryKey: .isGreaterThan),
             .init(key: "latitude", value: northEast.latitude, queryKey: .isLessThan)
         ]
-        
-        let service = self.dependencies.firestoreService
-        let latitudeFilteredChatRooms = service.fetchList(dataKey: .chatRoom, queryList: queryList) as Single<[ChatRoom]>
+        let latitudeFilteredChatRooms: Single<[ChatRoom]> = service.fetchList(dataKey: .chatRoom, queryList: queryList)
         
         return latitudeFilteredChatRooms
             .map {
                 $0.filter {
-                    guard let chatRoomLongitude = $0.location?.longitude else {
-                        return false
-                    }
+                    guard let chatRoomLongitude = $0.location?.longitude else { return false }
                     
                     return southWest.longitude < chatRoomLongitude && chatRoomLongitude < northEast.longitude
-                    
                 }
             }
     }
@@ -59,7 +55,20 @@ extension DefaultAccessibleChatRoomsRepository: AccessibleChatRoomsRepository {
     
     func fetchAccessibleDMChatRooms(in region: NCMapRegion) -> Single<[DMChatRoomListData]> {
         return self.fetchAccessibleAllChatRooms(in: region)
-        .map { $0.filter { $0.roomType == "group" } }
+        .map { $0.filter { $0.roomType == "dm" } }
         .map { $0.map { DMChatRoomListData(data: $0) } }
+    }
+    
+    // 더미
+    func fetchDummyChatRooms() -> Single<[ChatRoom]> {
+        let dummyChatRooms: [ChatRoom] = [
+            ChatRoom(location: NCLocation(longitude: 127.091, latitude: 37.32)),
+            ChatRoom(location: NCLocation(longitude: 127.092, latitude: 37.33)),
+            ChatRoom(location: NCLocation(longitude: 127.093, latitude: 37.34)),
+            ChatRoom(location: NCLocation(longitude: 127.094, latitude: 37.35)),
+            ChatRoom(location: NCLocation(longitude: 127.095, latitude: 37.36))
+        ]
+        
+        return Single.just(dummyChatRooms)
     }
 }
