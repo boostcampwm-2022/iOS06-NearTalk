cb714c69ce090f73c1832f3abf0127ea6023e02f
prestonk162 <prestonk162@gmail.com>
2022-12-01 11:06:23 +0900

    [Bug] 키보드 띄울 때 텍스트 필드 밑까지만 올리는 작업 진행 중

--- NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingView.swift
+++ NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingView.swift
@@ -11,6 +11,8 @@ import RxSwift
 import SnapKit
 import UIKit
 
+typealias KeyboardShowValues = (frame: CGRect, curve: Int, duration: Double)
+
 final class ProfileSettingView: UIView {
     // MARK: - UI properties
     private let profileImageView: UIImageView = UIImageView().then {
@@ -19,14 +21,34 @@ final class ProfileSettingView: UIView {
         $0.backgroundColor = .lightGray
     }
 
+    private let nickNameLabel: UILabel = UILabel().then {
+        $0.text = "닉네임"
+    }
+    
+    private let nickNameValidityMessageLabel: UILabel = UILabel().then {
+        $0.text = ""
+    }
+    
+    private let messageValidityMessageLabel: UILabel = UILabel().then {
+        $0.text = ""
+    }
+    
+    private let messageLabel: UILabel = UILabel().then {
+        $0.text = "상태메세지"
+    }
+    
     private let nicknameField: UITextField = UITextField().then {
         $0.placeholder = "닉네임"
-        $0.font = UIFont.systemFont(ofSize: 30)
+        $0.borderStyle = .roundedRect
+        $0.autocapitalizationType = .none
+        $0.clearButtonMode = .always
     }
     
     private let messageField: UITextField = UITextField().then {
         $0.placeholder = "상태 메세지"
-        $0.font = UIFont.systemFont(ofSize: 30)
+        $0.borderStyle = .roundedRect
+        $0.autocapitalizationType = .none
+        $0.clearButtonMode = .always
     }
     
     override init(frame: CGRect) {
@@ -48,7 +70,7 @@ final class ProfileSettingView: UIView {
     }
 }
 
-extension ProfileSettingView {
+extension ProfileSettingView {    
     var nickNameText: Observable<String> {
         self.nicknameField.rx.text
             .orEmpty
@@ -73,19 +95,166 @@ extension ProfileSettingView {
         return self.profileImageView.rx.image
             .asObserver()
     }
+    
+    private var keyboardWillShow: ControlEvent<Notification> {
+        return ControlEvent(events: NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillShowNotification))
+    }
+    
+    private var keyboardWillHide: ControlEvent<Notification> {
+        return ControlEvent(events: NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillHideNotification))
+    }
+    
+    var keyboardWillShowOnNickNameField: Driver<Notification> {
+        return self.keyboardWillShow.asDriver()
+            .filter { _ in
+                self.nicknameField.isFirstResponder
+            }
+    }
+    
+    var keyboardWillDismissFromNickNameField: Driver<Notification> {
+        return self.keyboardWillHide.asDriver()
+            .filter { _ in
+                self.nicknameField.isFirstResponder
+            }
+    }
+    
+    var keyboardWillShowOnMessageField: Driver<Notification> {
+        return self.keyboardWillShow.asDriver()
+            .filter { _ in
+                self.messageField.isFirstResponder
+            }
+    }
+    
+    var keyboardWillDismissFromMessageField: Driver<Notification> {
+        return self.keyboardWillHide.asDriver()
+            .filter { _ in
+                self.messageField.isFirstResponder
+            }
+    }
+    
+    func moveUpKeyboardAboveNickName(userInfo: [AnyHashable: Any]) {
+        print(#function)
+        
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let keyboardAnimationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int,
+              let keyboardDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+              let keyboardCurve = UIView.AnimationCurve(rawValue: keyboardAnimationCurve)
+        else {
+            return
+        }
+        
+        let keyboardSize: CGRect = keyboardFrame
+        let keyboardHeight: CGFloat = keyboardSize.height
+        
+        let newConstant = keyboardHeight - (self.frame.height - self.profileImageView.frame.height - self.nickNameLabel.frame.height - self.nicknameField.frame.height - self.nickNameValidityMessageLabel.frame.height - 50)
+        
+        self.profileImageView.snp.remakeConstraints { make in
+            make.horizontalEdges.equalTo(self.safeAreaLayoutGuide)
+            make.height.equalTo(profileImageView.snp.width)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(-newConstant)
+        }
+        
+        self.messageLabel.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(nickNameValidityMessageLabel.snp.bottom).offset(30 + keyboardHeight)
+        }
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardDuration, curve: keyboardCurve) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
+    
+    func moveUpKeyboardAboveMessage(userInfo: [AnyHashable: Any]) {
+        print(#function)
+        
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let keyboardAnimationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int,
+              let keyboardDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+              let keyboardCurve = UIView.AnimationCurve(rawValue: keyboardAnimationCurve)
+        else {
+            return
+        }
+        
+        let keyboardSize: CGRect = keyboardFrame
+        let keyboardHeight: CGFloat = keyboardSize.height
+        let newConstant = keyboardHeight
+        
+        self.profileImageView.snp.remakeConstraints { make in
+            make.horizontalEdges.equalTo(self.safeAreaLayoutGuide)
+            make.height.equalTo(profileImageView.snp.width)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(-newConstant)
+        }
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardDuration, curve: keyboardCurve) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
+
+    func moveDownKeyboard(userInfo: [AnyHashable: Any]) {
+        print(#function)
+        self.endEditing(true)
+        guard let keyboardAnimationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int,
+              let keyboardDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+              let keyboardCurve = UIView.AnimationCurve(rawValue: keyboardAnimationCurve)
+        else {
+            return
+        }
+        
+        self.profileImageView.snp.remakeConstraints { make in
+            make.horizontalEdges.top.equalTo(self.safeAreaLayoutGuide)
+            make.height.equalTo(profileImageView.snp.width)
+        }
+
+        self.messageLabel.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(nickNameValidityMessageLabel.snp.bottom).offset(30)
+        }
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardDuration, curve: keyboardCurve) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
+    
+    var nickNameValidityMessage: AnyObserver<String?> {
+        self.nickNameValidityMessageLabel.rx.text
+            .asObserver()
+    }
+    
+    var nickNameValidityColor: AnyObserver<UIColor> {
+        self.nickNameValidityMessageLabel.rx.textColor
+            .asObserver()
+    }
+    
+    var messageValidityMessage: AnyObserver<String?> {
+        self.messageValidityMessageLabel.rx.text
+            .asObserver()
+    }
+    
+    var messageValidityColor: AnyObserver<UIColor> {
+        self.messageValidityMessageLabel.rx.textColor
+            .asObserver()
+    }
 }
 
 private extension ProfileSettingView {
     func addSubViews() {
-        [profileImageView, nicknameField, messageField].forEach {
+        [profileImageView, nickNameLabel, nicknameField, nickNameValidityMessageLabel, messageLabel, messageField, messageValidityMessageLabel].forEach {
             self.addSubview($0)
         }
     }
     
     func configureConstraints() {
         self.configureProfileImageView()
-        self.configureNickNameField()
-        self.configureMessageField()
+        self.configureNickNameSection()
+        self.configureMessageSection()
     }
     
     func configureProfileImageView() {
@@ -95,19 +264,39 @@ private extension ProfileSettingView {
         }
     }
     
-    func configureNickNameField() {
-        nicknameField.snp.makeConstraints { (make) in
+    func configureNickNameSection() {
+        nickNameLabel.snp.makeConstraints { make in
             make.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
-            make.top.equalTo(profileImageView.snp.bottom).offset(30)
+            make.top.equalTo(self.profileImageView.snp.bottom).offset(30)
+        }
+        
+        nicknameField.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalTo(nickNameLabel)
+            make.top.equalTo(nickNameLabel.snp.bottom).offset(10)
             make.height.equalTo(nicknameField.snp.width).multipliedBy(0.15)
+        }
+        
+        nickNameValidityMessageLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(nickNameLabel)
+            make.top.equalTo(nicknameField.snp.bottom).offset(10)
         }
     }
     
-    func configureMessageField() {
-        messageField.snp.makeConstraints { (make) in
+    func configureMessageSection() {
+        messageLabel.snp.makeConstraints { make in
             make.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
-            make.top.equalTo(nicknameField.snp.bottom).offset(30)
+            make.top.equalTo(self.nickNameValidityMessageLabel.snp.bottom).offset(30)
+        }
+        
+        messageField.snp.makeConstraints { (make) in
+            make.horizontalEdges.equalTo(messageLabel)
+            make.top.equalTo(messageLabel.snp.bottom).offset(10)
             make.height.equalTo(messageField.snp.width).multipliedBy(0.15)
+        }
+        
+        messageValidityMessageLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(messageLabel)
+            make.top.equalTo(messageField.snp.bottom).offset(10)
         }
     }
 }
--- NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingViewController.swift
+++ NearTalk/NearTalk/Presentation/ProfileSetting/View/ProfileSettingViewController.swift
@@ -12,22 +12,51 @@ import SnapKit
 import Then
 import UIKit
 
-final class ProfileSettingViewController: UIViewController {
+final class ProfileSettingViewController: UIViewController, UIScrollViewDelegate {
     // MARK: - UI properties
-    private lazy var rootView: ProfileSettingView = ProfileSettingView()
+    private let rootView: ProfileSettingView = ProfileSettingView()
+    private let scrollView: UIScrollView = UIScrollView().then {
+        $0.keyboardDismissMode = .onDrag
+        $0.alwaysBounceHorizontal = false
+    }
     
     // MARK: - Properties
     private let disposeBag: DisposeBag = DisposeBag()
     private let viewModel: any ProfileSettingViewModel
 
     // MARK: - Lifecycles
+    override func viewWillLayoutSubviews() {
+//        self.rootView.frame = self.view.frame
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        let subViewsheight: CGFloat = self.rootView.subviews.reduce(0, { partialResult, view in
+//            partialResult + view.frame.height
+//        })
+//        let height: CGFloat = self.view.frame.height + subViewsheight
+//        self.scrollView.contentSize = .init(width: self.view.frame.width, height: height)
+        self.scrollView.updateContentSize()
+    }
+    
     override func loadView() {
-        self.view = self.rootView
+        self.view = self.scrollView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureNavigationBar()
+        self.scrollView.addSubview(self.rootView)
+        self.rootView.snp.makeConstraints { make in
+            make.edges.equalTo(self.scrollView.contentLayoutGuide)
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().priority(.low)
+        }
+//        self.scrollView.snp.makeConstraints { make in
+//            make.edges.width.equalTo(self.view.safeAreaLayoutGuide)
+//        }
+        
         self.bindToViewModel()
     }
     
@@ -42,18 +71,23 @@ final class ProfileSettingViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollView.contentOffset.x = 0
+    }
 }
 
 // MARK: - Helpers
 private extension ProfileSettingViewController {
     
     func configureNavigationBar() {
-//        self.navigationController?.navigationBar.backgroundColor = .systemGray5
-//        self.navigationController?.navigationBar.isTranslucent = false
         self.navigationItem.title = "프로필 설정"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "등록", style: .plain, target: self, action: nil)
         self.navigationItem.largeTitleDisplayMode = .automatic
-//        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .bold)]
+        if let navigationBar = self.navigationController?.navigationBar {
+            self.rootView.bringSubviewToFront(navigationBar)
+        }
+        self.navigationController?.isNavigationBarHidden = false
     }
     
     func bindToViewModel() {
@@ -67,8 +101,29 @@ private extension ProfileSettingViewController {
         self.rootView.nickNameText
             .bind(onNext: { [weak self] text in
                 self?.viewModel.editNickName(text)
-                
             })
+            .disposed(by: self.disposeBag)
+        self.viewModel.nickNameValidity
+            .asDriver()
+            .map { isValid in
+                isValid ? "사용 가능한 닉네임 입니다" : "5-16 자 사이의 영어 소문자, 숫자, -_ 기호만 사용하십시오"
+            }
+            .drive(self.rootView.nickNameValidityMessage)
+            .disposed(by: self.disposeBag)
+        self.viewModel.nickNameValidity
+            .asDriver()
+            .map { isValid in
+                isValid ? UIColor.green : UIColor.red
+            }
+            .drive(self.rootView.nickNameValidityColor)
+            .disposed(by: self.disposeBag)
+        self.rootView.keyboardWillShowOnNickNameField
+            .compactMap { $0.userInfo }
+            .drive(onNext: { self.rootView.moveUpKeyboardAboveNickName(userInfo: $0) })
+            .disposed(by: self.disposeBag)
+        self.rootView.keyboardWillDismissFromNickNameField
+            .compactMap { $0.userInfo }
+            .drive(onNext: { self.rootView.moveDownKeyboard(userInfo: $0) })
             .disposed(by: self.disposeBag)
     }
     
@@ -77,6 +132,28 @@ private extension ProfileSettingViewController {
             .bind(onNext: { [weak self] text in
                 self?.viewModel.editStatusMessage(text)
             })
+            .disposed(by: self.disposeBag)
+        self.rootView.keyboardWillShowOnMessageField
+            .compactMap { $0.userInfo }
+            .drive(onNext: { self.rootView.moveUpKeyboardAboveMessage(userInfo: $0) })
+            .disposed(by: self.disposeBag)
+        self.rootView.keyboardWillDismissFromMessageField
+            .compactMap { $0.userInfo }
+            .drive(onNext: { self.rootView.moveDownKeyboard(userInfo: $0) })
+            .disposed(by: self.disposeBag)
+        self.viewModel.messageValidity
+            .asDriver()
+            .map { isValid in
+                isValid ? "사용 가능한 메세지 입니다" : "50자 이하로 작성하십시오"
+            }
+            .drive(self.rootView.messageValidityMessage)
+            .disposed(by: self.disposeBag)
+        self.viewModel.messageValidity
+            .asDriver()
+            .map { isValid in
+                isValid ? UIColor.green : UIColor.red
+            }
+            .drive(self.rootView.messageValidityColor)
             .disposed(by: self.disposeBag)
     }
     
@@ -106,5 +183,26 @@ private extension ProfileSettingViewController {
                 })
                 .disposed(by: self.disposeBag)
         }
+    }
+}
+
+extension UIScrollView {
+    func updateContentSize() {
+        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
+        
+        // 계산된 크기로 컨텐츠 사이즈 설정
+        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
+    }
+    
+    private func recursiveUnionInDepthFor(view: UIView) -> CGRect {
+        var totalRect: CGRect = .zero
+        
+        // 모든 자식 View의 컨트롤의 크기를 재귀적으로 호출하며 최종 영역의 크기를 설정
+        for subView in view.subviews {
+            totalRect = totalRect.union(recursiveUnionInDepthFor(view: subView))
+        }
+        
+        // 최종 계산 영역의 크기를 반환
+        return totalRect.union(view.frame)
     }
 }
