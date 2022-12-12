62513b2b9ad9e3c3249a916b27882706a541e845
lymchgmk <lymchgmk@gmail.com>
2022-12-12 14:33:58 +0900

    [Feature] Utils ColorSet 추가

--- NearTalk/NearTalk/Infrastructure/Utils/ColorSet.swift
+++ NearTalk/NearTalk/Infrastructure/Utils/ColorSet.swift
@@ -9,11 +9,15 @@ import UIKit
 
 extension UIColor {
     static let primaryColor: UIColor? = UIColor(named: "primaryColor")
+    static let secondaryColor: UIColor? = UIColor(named: "secondaryColor")
+    
     static let label: UIColor? = UIColor(named: "label")
     static let secondaryLabel: UIColor? = UIColor(named: "secondaryLabel")
     static let tertiaryLabel: UIColor? = UIColor(named: "tertiaryLabel")
     static let whiteLabel: UIColor? = UIColor(named: "whiteLabel")
-    static let secondaryBackground: UIColor? = UIColor(named: "secondaryBackground")
+    
     static let primaryBackground: UIColor? = UIColor(named: "primaryBackground")
+    static let secondaryBackground: UIColor? = UIColor(named: "secondaryBackground")
+    
     static let seperatorColor: UIColor? = UIColor(named: "seperatorColor")
 }
